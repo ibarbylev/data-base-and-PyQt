0fcfaf18c53ee36ce74129cf9a6e0f92c963cdee
su <ibarbylev@gmail.com>
2021-11-14 04:13:45 +0200

    fix lesson 7

---
+++ lesson_7/examples/pep_8_ex/ex_5.py
@@ -0,0 +1,23 @@
+# Модули
+server
+my_module
+
+# Пакеты
+scripts
+
+# Классы
+LikeThis
+
+# Исключения
+LikeThis
+
+# Переменные и аргументы
+like_this
+
+# Имена функций и методов
+like_this
+likeThis
+
+# Константы
+MAX_OVERFLOW
+TOTAL

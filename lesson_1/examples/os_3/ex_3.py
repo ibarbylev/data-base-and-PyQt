67349d2e38532fce2b570f6c1ec4e5f4a9682abf
su <ibarbylev@gmail.com>
2021-11-18 07:32:46 +0200

    fix lesson 1

--- lesson_1/examples/os_3/ex_3.py
+++ lesson_1/examples/os_3/ex_3.py
@@ -7,37 +7,42 @@ import os
 # когда нужно использовать имя файла для наименования того
 # или иного связанного с работой файла, например лог-файл.
 # Такая ситуация возникает часто при работе с файлами данных.
-CUR_F = os.path.basename("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                         "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py")
+CUR_F = os.path.basename(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3/ex_3.py")
 print(CUR_F)
 
 # Функция dirname возвращает только часть каталога пути.
 # В данном примере мы просто возвращаем путь к каталогу.
 # Это также полезно, когда вам нужно сохранить другие файлы рядом с тем,
 # который вы обрабатываете в данный момент. Как и в случае с лог-файлом, упомянутым выше.
-CUR_D_PATH = os.path.dirname("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                             "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py")
+CUR_D_PATH = os.path.dirname(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3/ex_3.py")
 print(CUR_D_PATH)
 
 # Функция exists говорит нам, существует ли файл, или нет.
-F_EXISTS = os.path.exists("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                          "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py")
-print(F_EXISTS)
+print('is exists: ', os.path.exists(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/ipaddress_2"))
 
-print(os.path.isfile("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                     "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py"))
-print(os.path.isdir("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                    "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py"))
+# Функция isfile говорит нам, явдяется ли объект файлом
+print('isfile: ', os.path.isfile(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3/ex_3.py"))
+
+# Функция isdir говорит нам, явдяется ли объект файлом
+print('isdir: ', os.path.isdir(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3/ex_3.py"))
 
 # Метод join позволяет вам совместить несколько путей при помощи присвоенного разделителя.
 # К примеру, в Windows, в роли разделителя выступает бэкслэш (косая черта, указывающая назад),
 # однако в Linux функция разделителя присвоена косой черте, указывающей вперед (forward slash).
-print(os.path.join("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                   "Часть 2\Урок-1\Коды к методичке_МОИ\os", "3.py"))
+print(os.path.join(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3",
+    "3.py"))
 
-# Метод split разъединяет путь на кортеж, который содержит и файл и каталог.
-print(os.path.split("F:\Курсы GeekBrains\Python. Продвинутый уровень. "
-                    "Часть 2\Урок-1\Коды к методичке_МОИ\os\/3.py"))
+# Метод split разъединяет путь на tuple, который содержит и файл и каталог.
+print(os.path.split(
+    "/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_1/examples/os_3/ex_3.py"))
 
+# Метод listdir выдаёт список файлов и папок текущей директории.
 DIR_STRUCT = os.listdir('.')
+print('current dir: ', os.getcwd())
 print(DIR_STRUCT)

46f956c5d1d84c886b18fe5e8667fa5347b1c855
su <ibarbylev@gmail.com>
2021-12-06 14:25:25 +0200

    lesson 5 fix

---
+++ addditional materials/lesson_5_ex_5.py
@@ -0,0 +1,37 @@
+"""Работа с БД через PyQt"""
+
+
+"""
+Пока некогда долго разбираться, поэтому упростил максимально.
+TODO - сдалать бОльше примеров!!!
+"""
+
+
+import sys
+from PyQt5 import QtWidgets, QtSql
+from PyQt5.QtSql import QSqlQuery, QSqlDatabase
+
+# QTableView, QTreeView, QListView
+
+# Открытие базы данных SQLite
+con1 = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+db_path = '/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_5/examples/demo.sqlite'
+con1.setDatabaseName(db_path)
+print(con1.isOpen())
+con1.open()
+print(con1.isOpen())
+print(con1.databaseName())
+query = QSqlQuery()
+# !!!!   добавить пример от сюда: https://realpython.com/python-pyqt-database/ !!!!
+# query.exec("SELECT Name FROM Artist ORDER BY Name LIMIT 3")  # заменить SELECT на INSERT!!!
+query.exec("INSERT INTO Artist (Name) VALUES ('kkk')")  # заменить SELECT на INSERT!!!
+con1.close()
+
+# Открытие базы данных MySQL
+# con2 = QtSql.QSqlDatabase.addDatabase('QMYSQL')
+# con2.setHostName('localhost')
+# con2.setDatabaseName('TestDB')
+# con2.setUserName('Mario')
+# con2.setPassword('karabuka')
+# con2.open()
+# con2.close()
--- lesson_5/examples/ex_5.py
+++ lesson_5/examples/ex_5.py
@@ -8,22 +8,14 @@ from PyQt5.QtSql import QSqlQuery, QSqlDatabase
 
 # Открытие базы данных SQLite
 con1 = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-db_path = '/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_3/examples/demo.sqlite'
+db_path = '/home/su/Projects/GeekBrains/data-base-and-PyQt/lesson_5/examples/demo.sqlite'
 con1.setDatabaseName(db_path)
 print(con1.isOpen())
 con1.open()
 print(con1.isOpen())
 print(con1.databaseName())
 query = QSqlQuery()
-# !!!!   добавить пример от сюда: https://realpython.com/python-pyqt-database/ !!!!
-query.exec("SELECT Name FROM Artist ORDER BY Name LIMIT 3")  # заменить SELECT на INSERT!!!
+# Добавляем нового артиста
+query.exec("INSERT INTO Artist (Name) VALUES ('The New Artist')")
 con1.close()
 
-# Открытие базы данных MySQL
-# con2 = QtSql.QSqlDatabase.addDatabase('QMYSQL')
-# con2.setHostName('localhost')
-# con2.setDatabaseName('TestDB')
-# con2.setUserName('Mario')
-# con2.setPassword('karabuka')
-# con2.open()
-# con2.close()

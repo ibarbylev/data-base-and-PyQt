094c7d503e0889bf97354e9022a158442bb7f6b6
su <ibarbylev@gmail.com>
2021-12-13 14:44:14 +0200

    fix lesson 8

--- lesson_8/practice/server/setup.py
+++ lesson_8/practice/server/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name="mess_server",
-      version="0.8.3",
-      description="mess_server",
+setup(name="py_mess_server",
+      version="0.0.1",
+      description="Mess Server",
       author="Ivan Ivanov",
       author_email="iv.iv@yandex.ru",
       packages=find_packages(),

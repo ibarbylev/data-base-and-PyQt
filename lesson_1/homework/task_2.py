0d5f2db06b4ca2f4f7372860706266b07ca6603a
su <ibarbylev@gmail.com>
2021-12-14 17:25:24 +0200

    fix lesson 1

--- lesson_1/homework/task_2.py
+++ lesson_1/homework/task_2.py
@@ -30,8 +30,8 @@ def host_range_ping(get_list=False):
         if not end_ip.isnumeric():
             print("Необходимо ввести число")
         else:
-            if (last_oct + int(end_ip)) > 255:  # По условию меняется только последний октет
-                print(f"Можем менять только послдений октет, т.е. максимальное число хостов {255 - last_oct}")
+            if (last_oct + int(end_ip)) > 255+1:  # По условию меняется только последний октет
+                print(f"Можем менять только послдений октет, т.е. максимальное число хостов {255+1 - last_oct}")
             else:
                 break
     host_list = []

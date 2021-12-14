cb244d8cb6f9c3910f066b602c26f7ffde9a50c5
su <ibarbylev@gmail.com>
2021-12-14 16:21:15 +0200

    fix lesson 1

---
+++ addditional materials/lesson_1_asyncio_6/async_socket.py
@@ -0,0 +1,36 @@
+import asyncio
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('', 7777))
+server_socket.listen()
+# server_socket.setblocking(False)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setblocking(False)
+
+
+async def server_work():
+    client, address = await server_socket.accept()
+    msg = await client.recv(1024)
+    print('msg from client: ', msg.decode('utf-8'))
+    client.send('Hi, client!'.encode('utf-8'))
+    client.close()
+
+
+async def client_work():
+    client_socket.connect(('127.0.0.1', 7777))
+    client_socket.send('Hi, server'.encode('utf-8'))
+    msg = await client_socket.recv(1024)
+    print('msg from server: ', msg.decode('utf-8'))
+    client_socket.close()
+
+
+async def main():
+    task1 = asyncio.create_task(server_work())
+    task2 = asyncio.create_task(client_work())
+    await asyncio.gather(task1, task2)
+
+if __name__ == '__main__':
+    asyncio.run(main())

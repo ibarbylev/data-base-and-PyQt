86c15695d65cdd80e464d2428c055255fd4fd4b5
su <ibarbylev@gmail.com>
2021-12-18 23:10:53 +0200

    fix lesson 3

--- lesson_3/examples/exp_6_CRUD.py
+++ lesson_3/examples/exp_6_CRUD.py
@@ -13,19 +13,22 @@ from exp_6 import User, sess
 
 
 # create
-user_2 = User('Пётр', 'Петров', 'pass_Petrov')
+user_2 = User("Пётр", "Петров", "pass_Petrov")
+user_3 = user_2
+user_4 = user_2
 sess.add(user_2)
+sess.add(user_3)
+sess.add(user_4)
 sess.commit()
 
 # # Retrieve
 result = sess.query(User).filter_by(name='Пётр')
-print('total rows: ', result.count())
-print(result.all())
-#
+print('rows count: ', result.count())
+print('rows data: ', result.all())
+
 # # Update 1 row
 # result.first().password += '1'
 # sess.commit()
-#
 #
 # # Update multiple lines simultaneously var #1
 # for row in result.all():
@@ -37,6 +40,8 @@ print(result.all())
 # sess.commit()
 #
 # # Delete
+# print('count before delete: ', result.count())
+# print('rows before delete: ', result.all())
 # result.delete()
 # sess.commit()
 

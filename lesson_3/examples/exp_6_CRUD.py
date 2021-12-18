eb152bed2c857d3202c1ac46463cdd3fbc185fbc
su <ibarbylev@gmail.com>
2021-12-18 22:42:13 +0200

    fix lesson 3

--- lesson_3/examples/exp_6_CRUD.py
+++ lesson_3/examples/exp_6_CRUD.py
@@ -1,40 +1,42 @@
-from sqlalchemy import __version__, create_engine, Table, Column, \
-    Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm import mapper, sessionmaker
+"""
+Для того, чтобы свободно чувствовать себя с любой БД
+необходимо овладеть операциями CRUD:
+
+C - Create
+R - Retrieve
+U - Update
+D - Delete
+
+"""
 
 from exp_6 import User, sess
 
-# ******************* C R U D ******************
-# ******************* Create *******************
-# ******************* Retrieve *****************
-# ******************* Update *******************
-# ******************* Delete *******************
 
 # create
 user_2 = User('Пётр', 'Петров', 'pass_Petrov')
 sess.add(user_2)
 sess.commit()
 
-# # retrieve
+# # Retrieve
 result = sess.query(User).filter_by(name='Пётр')
 print('total rows: ', result.count())
 print(result.all())
 #
-# # update 1 row
+# # Update 1 row
 # result.first().password += '1'
 # sess.commit()
 #
 #
-# # update multiple lines simultaneously var #1
+# # Update multiple lines simultaneously var #1
 # for row in result.all():
 #     row.password += "1"
 # sess.commit()
 #
-# # update multiple lines simultaneously variant #2
+# # Update multiple lines simultaneously variant #2
 # result.update({User.password: 555})
 # sess.commit()
 #
-# # delete
+# # Delete
 # result.delete()
 # sess.commit()
 

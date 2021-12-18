54d9f1af0d5bd5654bca2e418967d5c2f1e0185e
su <ibarbylev@gmail.com>
2021-12-18 21:29:09 +0200

    fix lesson 3

--- lesson_3/examples/exp_1.py
+++ lesson_3/examples/exp_1.py
@@ -1,45 +1,52 @@
 """БД и запросы на выборку"""
 
-# Подключение библиотеки, соответствующей типу требуемой базы данных
+# Подключение библиотеки, соответствующей типу базы данных
 import os
 import sqlite3
 
-DB_OBJ = os.path.join(os.path.dirname(__file__), "demo.sqlite")
+db_full_path = os.path.join(os.path.dirname(__file__), "demo.sqlite")
 
 # Создание соединения с базой данных
 # В данном случае это файл базы
-CONN = sqlite3.connect(DB_OBJ)
-# CONN = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+connect = sqlite3.connect(db_full_path)
+# connect = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
 # Создаем курсор — это специальный объект,
 # который делает запросы и получает их результаты
-CURSOR = CONN.cursor()
+cursor = connect.cursor()
 
 # ---------------------------Запрос на выборку----------------------------- #
 
-CURSOR.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
 
-RESULTS = CURSOR.fetchall()  # [('A Aagrh!',), ('A Aagrh!',), ('A Aagrh!',)] - список кортежей
-RESULTS_2 = CURSOR.fetchall()  # [] - пустой список, т.к. результаты запроса уже обработаны
+# метод .fetchall() позволяет получить все данные сразу в виде списка
+# в котором каждая строка таблицы Artist представлена отдельным тюплом
+results = cursor.fetchall()  # [('A Aagrh!',), ('A Aagrh!',), ('A Aagrh!',)]
+print(results)
 
-print(RESULTS)
-print(RESULTS_2)
+# повторное обращение к объекту cursor вернёт пустой список,
+# т.к. результаты запроса уже обработаны
+results_2 = cursor.fetchall()  # []
+print(results_2)
 
-# или вот так
 
-# Получение результатов #
-CURSOR.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
-print(CURSOR.fetchone())  # обрабатываем результаты запроса - "вытаскиваем" по одному объекту
-print(CURSOR.fetchone())
-print(CURSOR.fetchone())
-print(CURSOR.fetchone())
+# Таким образом, cursor обладает свойствами итератора
+# и с помощью метода .fetchone() мы можем извлекать данные построчно
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT 3")
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
 
-# почему такой результат ?
 """
 ('A Cor Do Som',)
 ('AC/DC',)
 ('Aaron Copland & London Symphony Orchestra',)
 None
+
+---------------------------
+Как видим, последний запрос вернул None, означающий, 
+что все данные из объекта cursor уже извлечены
 """
 
-# потому к последнему шагу мы извлекли все объекты и вернулся None
+

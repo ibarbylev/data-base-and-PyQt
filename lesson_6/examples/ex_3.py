36b9281b9656cb9c7150d4f0d67917bc2ae24acb
su <root@root.com>
2022-01-11 01:41:02 +0200

    fix lesson 6

--- lesson_6/examples/ex_3.py
+++ lesson_6/examples/ex_3.py
@@ -23,9 +23,6 @@ from Cryptodome.Cipher import AES
 #  - блочные шифры: AES, DES, 3DES, Blowfish
 #  - поточные шифры: Salsa20, ChaCha20
 
-# шифруемое сообщение
-plaintext = b'The rain in Spain'
-
 
 def padding_text(text: bytes):
     """
@@ -52,13 +49,14 @@ def _encrypt(plaintext: bytes, key: bytes):
 
     cipher = AES.new(key, AES.MODE_CBC)
     # cipher - <Cryptodome.Cipher._mode_cbc.CbcMode object at 0x000000EFCDC8FE10>
-    ciphertext = cipher.iv + cipher.encrypt(plaintext)
+
+    ciphered_text = cipher.iv + cipher.encrypt(plaintext)
     # ciphertext - b'\xd2o\xba!\xa6\xd3i\xd2\x8a\x94m,\xcb\xaa\x14\x0c]\xa5\x88\xdd,
     # \x9by\xf6*l\xb3-\x93\xbb\xf8\x99ciYY\xa0\x07N\xcd\xd3GI\n%\xae0\xa5'
-    return ciphertext
+    return ciphered_text
 
 
-def _decrypt(ciphertext, key):
+def _decrypt(ciphered_text, key):
     """
     Расшифровка шифра ciphertext ключом key
 
@@ -66,14 +64,14 @@ def _decrypt(ciphertext, key):
     Его длина для большинства режимов шифрования всегда 16 байт.
     Расшифровываться будет оставшаяся часть шифра.
     """
-    # ciphertext - b'\xba\x0b\x0c\xad\x1f\x99\x12\'\xf11\xe3\x80\xfdl\xb9_\x01+\x9d\xd5\xc4\x00\x03J\xbb|
+    # ciphered_text - b'\xba\x0b\x0c\xad\x1f\x99\x12\'\xf11\xe3\x80\xfdl\xb9_\x01+\x9d\xd5\xc4\x00\x03J\xbb|
     # \x7f\x1d\x8bVz\xfeX\n\x8e\xb1"\xa2\x81$\x9a\x07\x11\xc9\xb6\xa30\xf6'
 
     # :16 - вектор инициализации
-    cipher = AES.new(key, AES.MODE_CBC, iv=ciphertext[:16])
+    cipher = AES.new(key, AES.MODE_CBC, iv=ciphered_text[:16])
     # cipher - <Cryptodome.Cipher._mode_cbc.CbcMode object at 0x000000036E6BB2B0>
     # 16: - само сообщение
-    msg = cipher.decrypt(ciphertext[16:])
+    msg = cipher.decrypt(ciphered_text[16:])
     # msg - b'The rain in Spain               '
 
     # убираем пробелы, добавленные ранее для соблюдения условия кратности 16-ти
@@ -84,17 +82,21 @@ def _decrypt(ciphertext, key):
 
 if __name__ == '__main__':
     # Осуществим шифрование сообщения алгоритмом AES
+    # шифруемое сообщение
+    plaintext = b'The rain in Spain'
+
     # key (строка байтов) - секретный ключ для симметричного шифрования.
     # Ключ должен быть длиной 16 (AES-128), 24 (AES-192) или 32 (AES-256) байта.
     key = b'Super Secret Key'
+    print('len(key) =', len(key))
 
     # Длина сообщения должна быть кратна 16, поэтому выполним выравнивание.
     plaintext = padding_text(plaintext)
 
     # Выполним шифрование
-    cipher = _encrypt(plaintext, key)
-    print('encrypted message: ', cipher)
+    ciphered_text = _encrypt(plaintext, key)
+    print('encrypted message: ', ciphered_text)
 
     # Выполним дешифрование
-    msg = _decrypt(cipher, key)
+    msg = _decrypt(ciphered_text, key)
     print('decrypted message: ', msg)

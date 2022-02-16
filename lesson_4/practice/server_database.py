cfaa91acc44fc355606ae9b498c9094ce6c63b67
su <root@root.com>
2022-02-16 15:20:03 +0200

    pep-8 for lesson 4-5

--- lesson_4/practice/server_database.py
+++ lesson_4/practice/server_database.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime
 from sqlalchemy.orm import mapper, sessionmaker
 from common.variables import *
@@ -264,13 +266,13 @@ if __name__ == '__main__':
     test_db = ServerStorage('server_base.db3')
     test_db.user_login('1111', '192.168.1.113', 8080)
     test_db.user_login('McG2', '192.168.1.113', 8081)
-    print(test_db.users_list())
-    print(test_db.active_users_list())
+    pprint(test_db.users_list())
+    pprint(test_db.active_users_list())
     test_db.user_logout('McG2')
-    print(test_db.login_history('re'))
+    pprint(test_db.login_history('re'))
     test_db.add_contact('test2', 'test1')
     test_db.add_contact('test1', 'test3')
     test_db.add_contact('test1', 'test6')
     test_db.remove_contact('test1', 'test3')
     test_db.process_message('McG2', '1111')
-    print(test_db.message_history())
+    pprint(test_db.message_history())

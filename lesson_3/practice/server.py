23a6013c45193d0753b9ed5732439551653c736d
ibarbylev <ibarbylev@gmail.com>
2022-03-31 19:11:30 +0300

    fix lesson 3

--- lesson_3/practice/server.py
+++ lesson_3/practice/server.py
@@ -63,6 +63,7 @@ class Server(threading.Thread, metaclass=ServerMaker):
             f' Если адрес не указан, принимаются соединения с любых адресов.')
         # Готовим сокет
         transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         transport.bind((self.addr, self.port))
         transport.settimeout(0.5)
 

a000b69ac19c7ff201cd2615ce13b4eaa79d0937
ibarbylev <ibarbylev@gmail.com>
2022-08-16 22:21:28 +0300

    fix server_database in lesson3

--- lesson_3/practice/server.py
+++ lesson_3/practice/server.py
@@ -199,16 +199,31 @@ def main():
         elif command == 'exit':
             break
         elif command == 'users':
-            for user in sorted(database.users_list()):
-                print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+            all_users = sorted(database.users_list())
+            if all_users:
+                for user in all_users:
+                    print(f'Пользователь {user[0]}, последний вход: {user[1]}')
+            else:
+                print('No data')
+
         elif command == 'connected':
-            for user in sorted(database.active_users_list()):
-                print(f'Пользователь {user[0]}, подключен: {user[1]}:{user[2]}, время установки соединения: {user[3]}')
+            active_users = sorted(database.active_users_list())
+            if active_users:
+                for user in active_users:
+                    print(f'Пользователь {user[0]}, подключен: {user[1]}:{user[2]}, '
+                          f'время установки соединения: {user[3]}')
+            else:
+                print('No data')
         elif command == 'loghist':
             name = input('Введите имя пользователя для просмотра истории. '
                          'Для вывода всей истории, просто нажмите Enter: ')
-            for user in sorted(database.login_history(name)):
-                print(f'Пользователь: {user[0]} время входа: {user[1]}. Вход с: {user[2]}:{user[3]}')
+            history = sorted(database.login_history(name))
+            if history:
+                for user in sorted(database.login_history(name)):
+                    print(f'Пользователь: {user[0]} время входа: {user[1]}. '
+                          f'Вход с: {user[2]}:{user[3]}')
+            else:
+                print('No data')
         else:
             print('Команда не распознана.')
 
